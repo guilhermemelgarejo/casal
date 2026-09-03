4230f8729c30e0b3e5f3b069798a88fc2ab630b5
Guilherme Volf Melgarejo <guilherme.melgarejo@gmail.com>
2026-09-02 23:36:14 -0300

    fix: executa backup via Schedule::call para evitar proc_open na hospedagem

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,7 @@ Artisan::command('inspire', function () {
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('db:backup-drive')->dailyAt('23:30');
+Schedule::call(function () {
+    Artisan::call('db:backup-drive');
+})->dailyAt('23:40')->name('db:backup-drive');
 
